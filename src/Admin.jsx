4c88052e7453f15b07eeacd3b4f7ae5fabe3a2cf
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-01-09 15:55:52 +0700

    Admin: add validation

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -4,60 +4,99 @@ function CreateUserForm({ employees, setEmployees }) {
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
   const [position, setPosition] = useState("");
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    lastname: "",
+    position: "",
+  });
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!name.trim()) {
+      errors.name = "Name can't be empty.";
+      isValid = false;
+    }
+
+    if (!lastname.trim()) {
+      errors.lastname = "Lastname can't be empty.";
+      isValid = false;
+    }
+
+    if (!position.trim()) {
+      errors.position = "Position can't be empty.";
+      isValid = false;
+    }
+
+    setFormErrors(errors);
+    return isValid;
+  };
 
   const handleCreate = (nextName, nextLastname, nextPosition) => {
-    setEmployees([
-      ...employees,
-      {
-        id: crypto.randomUUID(),
-        name: nextName,
-        lastname: nextLastname,
-        position: nextPosition,
-      },
-    ]);
+
+    if (validateForm()) {
+      setEmployees([
+        ...employees,
+        {
+          id: crypto.randomUUID(),
+          name: nextName,
+          lastname: nextLastname,
+          position: nextPosition,
+        },
+      ]);
+      setName("");
+      setLastname("");
+      setPosition("");
+    }
   };
 
   return (
     <div className="grid my-10">
       <div className="place-self-center">
         <h2 className="text-xl font-bold py-4 mx-4">Create User Here</h2>
-        <form onSubmit={(e) => {
-              e.preventDefault();
-              handleCreate(name, lastname, position)
-              setName("");
-              setLastname("");
-              setPosition("");
-            }}>
-          <input
-            className="p-2 mx-4 my-2 rounded"
-            type="text"
-            name="name"
-            placeholder="Name"
-            id="name"
-            value={name}
-            aria-label="name"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            className="p-2 mx-4 my-2 rounded"
-            type="text"
-            name="lastname"
-            placeholder="Last Name"
-            id="lastname"
-            value={lastname}
-            aria-label="lastname"
-            onChange={(e) => setLastname(e.target.value)}
-          />
-          <input
-            className="p-2 mx-4 my-2 rounded"
-            type="text"
-            name="position"
-            placeholder="Position"
-            id="position"
-            value={position}
-            aria-label="position"
-            onChange={(e) => setPosition(e.target.value)}
-          />
+        <form className="flex flex-row" onSubmit={(e) => {
+          e.preventDefault();
+          handleCreate(name, lastname, position)}}>
+          <div className="flex flex-col">
+            <input
+              className="p-2 mx-4 my-2 rounded"
+              type="text"
+              name="name"
+              placeholder="Name"
+              id="name"
+              value={name}
+              aria-label="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <p className="mx-4 text-sm text-red-500">{formErrors.name}</p>
+          </div>
+          <div className="flex flex-col">
+            <input
+              className="p-2 mx-4 my-2 rounded"
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              id="lastname"
+              value={lastname}
+              aria-label="lastname"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+            <p className="mx-4 text-sm text-red-500">{formErrors.lastname}</p>
+          </div>
+          <div className="flex flex-col">
+            <input
+              className="p-2 mx-4 my-2 rounded"
+              type="text"
+              name="position"
+              placeholder="Position"
+              id="position"
+              value={position}
+              aria-label="position"
+              onChange={(e) => setPosition(e.target.value)}
+            />
+            <p className="mx-4 text-sm text-red-500">{formErrors.position}</p>
+          </div>
           <button
             type="submit"
             className="py-2 px-6 mx-4 my-2 rounded bg-indigo-500 text-white"
